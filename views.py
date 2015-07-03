d1986fb406f03813ae28bf81137e0c8d3059d666
Kacper <random8902@gmail.com>
2015-07-03 02:27:30 +0200

    MVP 1.0 ScrumTask

--- views.py
+++ views.py
@@ -2,8 +2,8 @@ import sqlite3
 from functools import wraps
 
 from flask import Flask, flash, redirect, render_template, \
-    request, session, url_for
-
+    request, session, url_for, g
+from forms import AddTaskForm
 #Config
 app = Flask(__name__)
 app.config.from_object('_config')
@@ -26,9 +26,30 @@ def login_required(test):
     return wrap
 
 #Route handlers
+
+
 @app.route('/tasks')
+@login_required
 def tasks():
-    return "Stub"
+    g.db = connect_db()
+    cur = g.db.execute(
+        'SELECT name, due_date, priority, task_id FROM tasks where status=1'
+    )
+    open_tasks = g.db.execute(
+        'SELECT name, due_date, priority, task_id FROM tasks where status=0'
+    )
+    closed_tasks = [
+        dict(name=row[0], due_date=row[1], priority=row[2],
+             task_id=row[3]) for row in cur.fetchall()
+        ]
+    return render_template(
+        'tasks.html',
+        form=AddTaskForm(request.form),
+        open_tasks=open_tasks,
+        closed_tasks=closed_tasks
+    )
+    g.db.close()
+
 
 @app.route('/logout/')
 def logout():
@@ -36,17 +57,69 @@ def logout():
     flash('Goodbye!')
     return redirect(url_for('login'))
 
+
 @app.route('/', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         if request.form['username'] != app.config['USERNAME'] \
-               or request.form['password'] != app.config['PASSWORD']:
-                   error = 'Invalid Credentials. Please try again.'
-                   return render_template('login.html', error=error)
+                or request.form['password'] != app.config['PASSWORD']:
+                    error = 'Invalid Credentials. Please try again.'
+                    return render_template('login.html', error=error)
         else:
-           session['logged_in'] = True
-           flash('Welcome!')
-           return redirect(url_for('tasks'))
-
-
+            session['logged_in'] = True
+            flash('Welcome!')
+            return redirect(url_for('tasks'))
     return render_template('login.html')
+
+
+#CRUD section
+
+#Create
+@app.route('/add', methods=['POST'])
+@login_required
+def new_task():
+    g.db = connect_db()
+    name = request.form['name']
+    date = request.form['due_date']
+    priority = request.form['priority']
+    if not name or not date or not priority:
+        flash("All fields are required. Please try again.")
+        return redirect(url_for('tasks'))
+    else:
+        g.db.execute('INSERT INTO tasks(name, due_date, priority,status) \
+            values (?,?,?,1)', [
+            request.form['name'],
+            request.form['due_date'],
+            request.form['priority']]
+            )
+        g.db.commit()
+        g.db.close()
+        flash("New entry was posted.")
+        return redirect(url_for('tasks'))
+
+#Update
+
+
+@app.route('/complete/<int:task_id>')
+@login_required
+def complete(task_id):
+    g.db = connect_db()
+    g.db.execute(
+        'UPDATE tasks SET status = 0 WHERE task_id'+str(task_id)
+    )
+    g.db.commit()
+    g.db.close()
+    flash("The task was marked as complete.")
+    return redirect(url_for('tasks'))
+
+
+#Delete
+@app.route('/delete/<int:task_id>/')
+@login_required
+def delete_entry(task_id):
+    g.db = connect_db()
+    g.db.execute('DELETE FROM tasks WHERE task_id'+str(task_id))
+    g.db.commit()
+    g.db.close()
+    flash("The task was deleted")
+    return redirect(url_for('tasks'))
