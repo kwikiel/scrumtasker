28a1a7bf9feebe2f5428fe0d1c9af9b908f8caee
Kacper <random8902@gmail.com>
2015-07-03 23:30:55 +0200

    Fixed SQL queries

--- views.py
+++ views.py
@@ -105,7 +105,7 @@ def new_task():
 def complete(task_id):
     g.db = connect_db()
     g.db.execute(
-        'UPDATE tasks SET status = 0 WHERE task_id'+str(task_id)
+        'UPDATE tasks SET status = 0 WHERE task_id = '+str(task_id)
     )
     g.db.commit()
     g.db.close()
@@ -118,7 +118,7 @@ def complete(task_id):
 @login_required
 def delete_entry(task_id):
     g.db = connect_db()
-    g.db.execute('DELETE FROM tasks WHERE task_id'+str(task_id))
+    g.db.execute('DELETE FROM tasks WHERE task_id ='+str(task_id))
     g.db.commit()
     g.db.close()
     flash("The task was deleted")
