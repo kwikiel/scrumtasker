c52e7016fa855f9177cac3f30f16bf54fa7f40d1
Kacper Wikiel <random8902@gmail.com>
2015-08-09 00:09:53 +0200

    Migration: user connected to tasks

--- models.py
+++ models.py
@@ -9,6 +9,7 @@ class User(db.Model):
     name = db.Column(db.String, unique=True, nullable=False)
     email = db.Column(db.String, unique=True, nullable=False)
     password = db.Column(db.String, nullable=False)
+    tasks = db.relationship('Task', backref='poster')
 
     def __init__(self, name=None, email=None, password=None):
         self.name = name
@@ -22,10 +23,12 @@ class Task(db.Model):
 
     __tablename__ = "tasks"
 
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     task_id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
     due_date = db.Column(db.Date, nullable=False)
     priority = db.Column(db.Integer, nullable=False)
+    posted_date = db.Column(db.Date, default=datetime.datetime.utcnow())
     status = db.Column(db.Integer)
 
     def __init__(self, name, due_date, priority, status):
